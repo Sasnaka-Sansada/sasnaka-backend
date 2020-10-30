6ff5fea88dcc2c50fdf404e3e068c222c9d5f144
gayal <g.c.dassanayake@gmail.com>
2020-10-30 12:40:03 +0530

    removed project/list logging check

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { IsLoggedMiddleware } = require('../loaders/authenticator');
+// const { IsLoggedMiddleware } = require('../loaders/authenticator');
 const projectController = require('../api/project');
 
 router.get('/list/:pillerId', projectController.GetListProjectsOfAPiller);
-router.get('/list', IsLoggedMiddleware(), projectController.GetListProjects);
+router.get('/list', projectController.GetListProjects);
 router.post('/create', projectController.PostCreateProject);
 router.delete('/:id', projectController.DeleteDeleteProject);
 router.get('/:id', projectController.GetGetProject);
